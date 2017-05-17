61029d62786c832298c816a76b09dc8b4bdfa2bf
Kellie Bohlsen <Kellie Bohlsen@LAPTOP-F87J7B1L>
2017-05-17 14:26:54 +1000

    Add project files.

---
+++ TestGit3/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TestGit3
+{
+    [Activity(Label = "TestGit3", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
